2cf2a17751fc7e356289af4c69c75ddaff3ba79c
Tisha Dey <tishadey2025@outlook.com>
2026-05-13 21:39:17 +0600

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,5 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -20,9 +22,14 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
+      data-theme="light"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../assets/logo.png";
+
+export default function Navbar() {
+  return (
+    <div className="max-lg:collapse bg-emerald-50 lg:mb-48 shadow-sm w-full rounded-md">
+      <input id="navbar-1-toggle" className="peer hidden" type="checkbox" />
+      <label
+        htmlFor="navbar-1-toggle"
+        className="fixed inset-0 hidden max-lg:peer-checked:block"
+      ></label>
+      <div className="collapse-title navbar flex justify-between">
+        <div className="navbar-start">
+          <label htmlFor="navbar-1-toggle" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <Link href="/"><Image src={logo} alt="logo" width={70} height={70}></Image></Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 text-emerald-500 font-bold">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/campaigns">All campains</Link>
+            </li>
+            <li>
+              <Link href="/dashboard">Dashboard</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
